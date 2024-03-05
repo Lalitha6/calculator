e51ea5861728a4663aa0570523866aad7038e404
Lalitha6 <152023377+Lalitha6@users.noreply.github.com>
2024-03-05 12:15:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+let displayValue = '';
+
+function appendNumber(number) {
+  displayValue += number;
+  updateDisplay();
+}
+
+function appendOperator(operator) {
+  displayValue += operator;
+  updateDisplay();
+}
+
+function clearDisplay() {
+  displayValue = '';
+  updateDisplay();
+}
+
+function calculate() {
+  try {
+    displayValue = eval(displayValue);
+    updateDisplay();
+  } catch (error) {
+    alert('Error in calculation');
+    clearDisplay();
+  }
+}
+
+function updateDisplay() {
+  document.getElementById('display').value = displayValue;
+}
